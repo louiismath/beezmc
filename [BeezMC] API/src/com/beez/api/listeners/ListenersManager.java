013e73f1ed609efa97218838d7d13edd05f486e5
louiismath <lmathfournier@outlook.com>
2020-02-23 15:03:00 -0500

    Initial commit

--- [BeezMC] API/src/com/beez/api/listeners/ListenersManager.java
+++ [BeezMC] API/src/com/beez/api/listeners/ListenersManager.java
@@ -8,6 +8,7 @@ import com.beez.api.listeners.chat.PlayerChatListener;
 import com.beez.api.listeners.connect.PlayerJoinListener;
 import com.beez.api.listeners.connect.PlayerPreJoinListener;
 import com.beez.api.listeners.connect.PlayerQuitListener;
+import com.beez.api.listeners.move.PlayerMoveListener;
 
 public class ListenersManager {
 	
@@ -24,6 +25,7 @@ public class ListenersManager {
 		pluginManager.registerEvents(new PlayerJoinListener(), plugin);
 		pluginManager.registerEvents(new PlayerQuitListener(), plugin);
 		pluginManager.registerEvents(new PlayerChatListener(), plugin);
+		pluginManager.registerEvents(new PlayerMoveListener(), plugin);
 	}
 
 }
---
+++ [BeezMC] API/src/com/beez/api/listeners/move/PlayerMoveListener.java
@@ -0,0 +1,7 @@
+package com.beez.api.listeners.move;
+
+import org.bukkit.event.Listener;
+
+public class PlayerMoveListener implements Listener {
+
+}
